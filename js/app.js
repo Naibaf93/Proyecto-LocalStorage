11462c4da633fdcd8b6d7351e98c557906387d69
Fabián Huertas <miloh9318@hotmail.com>
2024-03-19 19:52:36 -0500

    validando el formulario

--- js/app.js
+++ js/app.js
@@ -16,5 +16,32 @@ function eventListeners() {
 
 function agregarTweet(e) {
     e.preventDefault();
-    console.log('Agregando tweet...');
+    
+    // Text area donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validacion   
+    if(tweet === '') {
+        mostrarError('no puede estar vacio');
+
+        return; // Evita que se ejecuten mas lienas de codigo
+    }
+}
+
+// Mostrar mensaje de error
+
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertarlo en el contenido 
+
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Eliminar la alerta despues de 3 seg
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
