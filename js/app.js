95786b0dc62e2b329803000ca99d192bfbe67de2
Fabián Huertas <miloh9318@hotmail.com>
2024-03-20 19:36:43 -0500

    mostrar los tweets

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Variables
 
 const formulario = document.querySelector('#formulario');
-const listaTweets = document.querySelector('lista-tweets');
+const listaTweets = document.querySelector('#lista-tweets');
 let tweets = [];
 
 
@@ -26,6 +26,20 @@ function agregarTweet(e) {
 
         return; // Evita que se ejecuten mas lienas de codigo
     }
+
+    const tweetObj = {
+        id: Date.now(),
+        tweet
+    }
+
+    // Añadir arreglo de tweets
+    tweets = [...tweets, tweetObj];
+
+    // Una vez agregado vamos a crear el html
+    crearHtml();
+
+    // Reinciar el formulario
+    formulario.reset();
 }
 
 // Mostrar mensaje de error
@@ -44,4 +58,31 @@ function mostrarError(error) {
     setTimeout(() => {
         mensajeError.remove();
     }, 3000);
+}
+
+// Muestra un listado de los tweets
+function crearHtml() {
+
+    limpiarHTML();
+
+    if(tweets.length > 0) {
+        tweets.forEach( tweet => {
+            // Crear el HTML
+            const li = document.createElement('li');
+
+            //Añadir el texto
+            li.innerText = tweet.tweet;
+
+            // Insertarlo en el HTML
+            listaTweets.appendChild(li);    
+        });
+    }
+}
+
+// Limpiar el HTML
+
+function limpiarHTML() {
+    while (listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
